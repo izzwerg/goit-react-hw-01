7031f74276497cdcfc717ada4d4182d2b90da07c
izzwerg <izzwerg.kr@gmail.com>
2024-02-25 16:22:43 +0200

    profile

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Profile from "./components/Profile/Profile.jsx";
+// import FriendList from './components/FriendList/FriendList.jsx';
+// import TransactionHistory from './components/TransactionHistory/TransactionHistory.jsx';
+
+import userData from './userData.json';
+// import friends from './friends.json';
+// import transactions from './transactions.json';
+
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        followers={userData.stats.followers}
+        views={userData.stats.views}
+        likes={userData.stats.likes}
+      />
+      {/* <FriendList friends={friends} />
+      <TransactionHistory items={transactions} /> */}
+    </>
+  );
+};
+
+export default App
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,43 @@
+import styles from './Profile.module.css';
+
+const Profile = ({
+  name,
+  tag,
+  location,
+  image,
+  followers,
+  views,
+  likes,
+}) => {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.userInfo}>
+        <img
+          width={150}
+          src={image}
+          alt="User avatar"
+          className={styles.avatar}
+        />
+        <h3 className={styles.userName}>{name}</h3>
+        <p className={styles.userTag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <span>Followers</span>
+          <span className={styles.itemAccent}>{followers}</span>
+        </li>
+        <li className={styles.item}>
+          <span>Views</span>
+          <span className={styles.itemAccent}>{views}</span>
+        </li>
+        <li className={styles.item}>
+          <span>Likes</span>
+          <span className={styles.itemAccent}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
